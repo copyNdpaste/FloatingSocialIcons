4728b34063b6cb816df7f164ea1672e4be9864ee
심명훈 <julsnutini@naver.com>
2017-07-19 11:43:36 +0900

    Initial commit

---
+++ app/src/main/java/com/example/mh/floatingsocialicons/MainActivity.java
@@ -0,0 +1,116 @@
+package com.example.mh.floatingsocialicons;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import com.github.clans.fab.FloatingActionButton;
+import com.github.clans.fab.FloatingActionMenu;
+
+public class MainActivity extends AppCompatActivity {
+
+    FloatingActionMenu materialDesignFAM;
+    FloatingActionButton floatingActionButton1, floatingActionButton2, floatingActionButton3, floatingActionButton4, floatingActionButton5, floatingActionButton6;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        materialDesignFAM = (FloatingActionMenu) findViewById(R.id.social_floating_menu);
+        floatingActionButton1 = (FloatingActionButton) findViewById(R.id.floating_facebook);
+        floatingActionButton2 = (FloatingActionButton) findViewById(R.id.floating_twitter);
+        floatingActionButton3 = (FloatingActionButton) findViewById(R.id.floating_kakaotalk);
+        floatingActionButton4 = (FloatingActionButton) findViewById(R.id.floating_instagram);
+       // floatingActionButton5 = (FloatingActionButton) findViewById(R.id.floating_instagram);
+      //  floatingActionButton6 = (FloatingActionButton) findViewById(R.id.floating_youtube);
+
+        floatingActionButton1.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                //TODO something when floating action menu first item clicked
+                Intent facebookIntent = getOpenFacebookIntent(MainActivity.this);
+                startActivity(facebookIntent);
+
+            }
+        });
+        floatingActionButton2.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                //TODO something when floating action menu second item clicked
+                Intent twitterIntent = getOpenTwitterIntent(MainActivity.this);
+                startActivity(twitterIntent);
+
+            }
+        });
+        floatingActionButton3.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                //TODO something when floating action menu third item clicked
+                Intent linkdinIntent = getOpenKakaotalkIntent(MainActivity.this);
+                startActivity(linkdinIntent);
+            }
+        });
+
+        floatingActionButton4.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                //TODO something when floating action menu first item clicked
+                Intent googleplusIntent = getOpenInstagramIntent(MainActivity.this);
+                startActivity(googleplusIntent);
+            }
+        });
+    }
+
+    public static Intent getOpenFacebookIntent(Context context) {
+
+        try {
+            context.getPackageManager()
+                    .getPackageInfo("com.facebook.katana", 0); //Checks if FB is even installed.
+            return new Intent(Intent.ACTION_VIEW,
+                    Uri.parse("fb://page/376227335860239")); //Trys to make intent with FB's URI
+        } catch (Exception e) {
+            return new Intent(Intent.ACTION_VIEW,
+                    Uri.parse("https://www.facebook.com/karthikofficialpage")); //catches and opens a url to the desired page
+        }
+    }
+
+    public static Intent getOpenTwitterIntent(Context context) {
+
+        try {
+            context.getPackageManager()
+                    .getPackageInfo("com.twitter.android", 0); //Checks if Twitter is even installed.
+            return new Intent(Intent.ACTION_VIEW,
+                    Uri.parse("https://twitter.com/drkarthiik")); //Trys to make intent with Twitter's's URI
+        } catch (Exception e) {
+            return new Intent(Intent.ACTION_VIEW,
+                    Uri.parse("https://twitter.com/drkarthiik")); //catches and opens a url to the desired page
+        }
+    }
+
+    public static Intent getOpenInstagramIntent(Context context) {
+
+        try {
+            context.getPackageManager()
+                    .getPackageInfo("com.instagram.android", 0); //Checks if Instagram is even installed.
+            return new Intent(Intent.ACTION_VIEW,
+                    Uri.parse("https://www.instagram.com/accounts/login/")); //Trys to make intent with Instagram's URI
+        } catch (Exception e) {
+            return new Intent(Intent.ACTION_VIEW,
+                    Uri.parse("https://www.instagram.com/accounts/login/")); //catches and opens a url to the desired page
+        }
+    }
+
+    public static Intent getOpenKakaotalkIntent(Context context) {
+
+        try {
+            context.getPackageManager()
+                    .getPackageInfo("com.google.android.youtube", 0); //Checks if YT is even installed.
+            return new Intent(Intent.ACTION_VIEW,
+                    Uri.parse("https://www.youtube.com/karthikm128")); //Trys to make intent with YT's URI
+        } catch (Exception e) {
+            return new Intent(Intent.ACTION_VIEW,
+                    Uri.parse("https://www.youtube.com/karthikm128")); //catches and opens a url to the desired page
+        }
+    }
+}
